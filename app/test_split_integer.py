8ff8da21b4caab6170822ff69ff97f60f32c61d4
fanghey <2603Lysenko2603@gmail.com>
2026-05-19 18:57:50 +0200

    Update test_split_integer.py

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,10 +2,11 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    value,number_of_parts = 17, 4
+    value, number_of_parts = 17, 4
     parts = split_integer(value, number_of_parts)
-    assert sum(parts) == 4
-    assert len(parts) == 17
+
+    assert sum(parts) == value
+    assert len(parts) == number_of_parts
     assert max(parts) - min(parts) <= 1
     assert parts == sorted(parts)
 
@@ -17,14 +18,16 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8, 1) == [8]
 
+
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    parts = split_integer(17,4)
+    parts = split_integer(17, 4)
     assert parts == sorted(parts)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     parts = split_integer(3, 5)
+
     assert len(parts) == 5
+    assert sum(parts) == 3
     assert max(parts) - min(parts) <= 1
-    assert sum(parts) == 5
     assert parts == sorted(parts)
